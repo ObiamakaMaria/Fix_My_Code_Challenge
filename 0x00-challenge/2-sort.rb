dc7431a95555b7a9ce4b1d67a5222e22e7f505d2
ObiamakaMaria <obiamaka0101@gmail.com>
2023-10-29 13:03:49 -0700

    2-sort.rb

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,31 @@
+###
+#
+#  This function Sort integer arguments in ascending order
+#
+###
+
+result = []
+ARGV.each do |arg|
+    # Function skips if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # converting  to integer
+    i_arg = arg.to_i
+    
+    # inserting result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+end
+
+puts result
